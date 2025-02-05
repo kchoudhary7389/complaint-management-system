46921b3bbecf61a5ecfed19c7a7fa7d39dc23afc
krishna choudhary <kchoudhary@gmail.com>
2025-02-05 20:52:11 +0530

    again cors changed

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: "http://localhost:5173", // Your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 app.use(express.json());
