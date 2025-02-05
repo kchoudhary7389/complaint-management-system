edc41da35a43643ba7489104139d75cabb54fb69
krishna choudhary <kchoudhary@gmail.com>
2025-02-05 21:22:58 +0530

    Fixed CORS issue

--- backend/app.js
+++ backend/app.js
@@ -8,20 +8,15 @@ const connectTodb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const adminRoutes = require("./routes/admin.route");
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN, 
-    credentials: true,
+    origin: ["http://localhost:5173"], // Add your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Allow cookies & authentication headers
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
