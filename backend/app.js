c62dc70884ee9203e05eb714038e58c9c31bfd49
krishna choudhary <kchoudhary@gmail.com>
2025-02-05 20:54:34 +0530

    headers added

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,14 @@ const connectTodb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const adminRoutes = require("./routes/admin.route");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(
   cors({
     origin: "http://localhost:5173", // Your frontend URL
